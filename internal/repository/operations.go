3c63c0df88e29b7d2969a8531fe2399979c11457
Khoa Tran <ovn6002@gmail.com>
2025-05-28 19:19:59 +0700

    feat(repository): implement AddToIndex operation

--- internal/repository/operations.go
+++ internal/repository/operations.go
@@ -1,7 +1,13 @@
 package repository
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
-func (r *Repository) AddToIndex(path, hash string, info os.FileInfo) error {
-	return nil
+func (repo *Repository) AddToIndex(path, hash string, info os.FileInfo) error {
+	if repo.index == nil {
+		return fmt.Errorf("index not initialized")
+	}
+	return repo.index.AddEntry(path, hash, info)
 }
