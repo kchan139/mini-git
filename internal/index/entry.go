81a9c050a621c9da19a66dee9196a8ed8e4f3d24
Khoa Tran <ovn6002@gmail.com>
2025-05-29 17:36:08 +0700

    refactor(index): move Entry struct to entry.go

--- internal/index/entry.go
+++ internal/index/entry.go
@@ -1,1 +1,15 @@
 package index
+
+import (
+	"os"
+	"time"
+)
+
+// Represents a file in the staging area
+type Entry struct {
+	Path    string      `json:"path"`
+	Hash    string      `json:"hash"`
+	Mode    os.FileMode `json:"mode"`
+	Size    int64       `json:"size"`
+	ModTime time.Time   `json:"mod_time"`
+}
--- internal/index/index.go
+++ internal/index/index.go
@@ -7,17 +7,7 @@ import (
 	"maps"
 	"os"
 	"path/filepath"
-	"time"
 )
-
-// Represents a file in the staging area
-type Entry struct {
-	Path    string      `json:"path"`
-	Hash    string      `json:"hash"`
-	Mode    os.FileMode `json:"mode"`
-	Size    int64       `json:"size"`
-	ModTime time.Time   `json:"mod_time"`
-}
 
 // Manages the staging area
 type Index struct {
